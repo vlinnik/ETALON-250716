aef799bd0501bc3726c88f549a72d7300be55752
Linnik V.V <vlinnik@mail.ru>
2026-09-07 17:14:10 +0300

    chores: uid для dashboards

--- src/gui/_version.py
+++ src/gui/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.post14+g2d18f1a5d.d20250903'
-__version_tuple__ = version_tuple = (0, 0, 'post14', 'g2d18f1a5d.d20250903')
+__version__ = version = '0.0.post15+g30b8175d5.d20250903'
+__version_tuple__ = version_tuple = (0, 0, 'post15', 'g30b8175d5.d20250903')
 
-__commit_id__ = commit_id = 'g2d18f1a5d'
+__commit_id__ = commit_id = 'g30b8175d5'
